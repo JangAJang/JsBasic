42c59e7490b8187e823ea0c417643a845df2406e
JangAJang <janghee5395@gmail.com>
2023-08-22 15:41:39 +0900

    feat: type ORM을 이용한 기본적인 멤버 객체 구현

---
+++ member.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("user")
+export class Member extends BaseEntity {
+  @PrimaryGeneratedColumn("user_id")
+  id: number;
+
+  @Column("user_firstName")
+  firstName: string;
+
+  @Column("user_lastName")
+  lastName: string;
+}
