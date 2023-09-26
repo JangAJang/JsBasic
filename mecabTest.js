7566eab73f69a91e6679495e7be1b012f0924ad1
JangAJang <janghee5395@gmail.com>
2023-09-26 15:13:31 +0900

    feat: 데이터를 계속해서 입력받아 mecab으로 자연어 처리를 한 후, 통계를 저장하고, end를 입력받으면 처리된 자연어를 사용된 횟수가 많은 순서로 조회하는 기능 구현

--- mecabTest.js
+++ mecabTest.js
@@ -1,19 +1,29 @@
 const mecab = require("mecab-ya");
 const readline = require("readline");
 const { stdin: input, stdout: output } = require("process");
+const DataMap = require("./dataMap");
+
+const nouns = new DataMap();
 
 const rl = readline.createInterface({ input, output });
 
 rl.on("line", (line) => {
+  if (line === "end") {
+    rl.close();
+    return;
+  }
+
   printParsedResult(line);
 });
 
 rl.on("close", () => {
+  console.log(nouns.getDatasOrderByCountDesc());
   process.exit();
 });
 
 const printParsedResult = (text) => {
   mecab.nouns(text, (err, result) => {
     console.log("nouns : ", result);
+    nouns.addData(result);
   });
 };
